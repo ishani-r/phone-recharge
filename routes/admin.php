817910e88f42cd1385158f6164d10558e6922bb2
Ishani <ranpariyaishani21@gmail.com>
2021-12-22 13:12:33 +0530

    22/12--2

--- routes/admin.php
+++ routes/admin.php
@@ -20,25 +20,7 @@ Route::group(['middleware' => 'auth:admin'], function () {
     Route::get('dashboard/content', function () {
         return view('admin.dashboard.content');
     })->name('main');
-    Route::get('notification', function () {
-        return view('admin.dashboard.notification');
-    })->name('notification');
-    Route::get('package', function () {
-        return view('admin.dashboard.package');
-    })->name('package')->middleware(['permission:create-package']);
-    Route::get('settings', function () {
-        return view('admin.dashboard.addteams');
-    })->name('addteams')->middleware(['permission:create-terms']);
-    Route::get('helps', function () {
-        return view('admin.dashboard.help');
-    })->name('help')->middleware(['permission:create-question-answer']);
-    // Route::get('packageget', function () {
-    //     return view('myPDF');
-    // });
-    Route::get('premium/status', 'Auth\PremiumController@status')->name('statuspermium');
-    Route::get('setting/status', 'Auth\SettingController@status')->name('statussetting');
-    Route::get('hel/status', 'Auth\HelpController@status')->name('statushelp');
-    // Route::view('dashboard/master','layouts.master');
+    
 
     // --------------------------------------Admin Profile-------------------------------------------
     Route::put('update-profile/{id}', 'Auth\ProfileController@updateProfile')->name('update_profile');
@@ -56,39 +38,9 @@ Route::group(['middleware' => 'auth:admin'], function () {
     Route::resource('dashboard', Auth\UserController::class)->middleware(['permission:view-user-table']);
     Route::resource('details', Auth\UserDetailsController::class)->middleware(['permission:view-userdetail-table']);
     
-    // ---------------------------------------- Like ---------------------------------------
-    Route::get('notification', 'Auth\LikeController@notification')->name('notification')->middleware(['permission:view-notification']);
-    Route::delete('delete/{id}', 'Auth\LikeController@destroy')->name('delete');
     
-    // ---------------------------------------- Premium Package ---------------------------------------
-    Route::resource('premium', Auth\PremiumController::class);
-    // ---------------------------------------- Setting ---------------------------------------
-    Route::resource('setting', Auth\Settingcontroller::class);
     
-    // ---------------------------------------- Localization ---------------------------------------
-    Route::get('index', 'Auth\LocalizationController@index')->name('language')->middleware(['permission:view-language']);
-    Route::get('change/lang', 'Auth\LocalizationController@langchange')->name('LangChange');
     
-    // ---------------------------------------- Help ---------------------------------------
-    Route::resource('help', Auth\HelpController::class);
-    
-    // ---------------------------------------- Contact US ---------------------------------------
-    Route::get('contactus', 'Auth\ContactController@contactUs')->name('contactus')->middleware(['permission:view-contacts-replay']);
-    Route::get('replay/{id}', 'Auth\ContactController@replay')->name('replay');
-    Route::post('replayuser/{id}', 'Auth\ContactController@replayUser')->name('replayuser');
-    Route::get('showreplay', 'Auth\ContactController@showreplay')->name('showreplay');
-    Route::post('deletereplaylist/{id}', 'Auth\ContactController@destroyReplayList')->name('deleteReplayList');
-    Route::post('deletereplay/{id}', 'Auth\ContactController@deleteReplay')->name('deleteReplay');
-    
-    // ---------------------------------------- Excal ---------------------------------------
-    Route::get('importExportView', [ImportExportController::class, 'importExportView']);
-    Route::get('export', [ImportExportController::class, 'export'])->name('export');
-    Route::post('import', [ImportExportController::class, 'import'])->name('import');
-    
-    // ---------------------------------------- PDF ---------------------------------------
-    Route::get('getAllPackage', 'Auth\PdfController@getAllPackage');
-    Route::get('exportPDF', 'Auth\PdfController@exportPDF')->name('exportpdf');
-    // Route::get('getAllPackage', 'Auth\PdfController@getAllPackage')->name('getpackage');
     
     // ---------------------------------------- Permission ---------------------------------------
     Route::resource('permission', Auth\PermissionController::class)->middleware(['permission:view-permission-table']);
